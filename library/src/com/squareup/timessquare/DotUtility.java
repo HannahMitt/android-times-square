4ccf0f7e52e464642898970d6b51dea9ec61ad1e
Hannah Mittelstaedt <hannahmittel@gmail.com>
2013-04-20 18:14:05 -0400

    Added Linn's colors

--- library/src/com/squareup/timessquare/DotUtility.java
+++ library/src/com/squareup/timessquare/DotUtility.java
@@ -7,10 +7,10 @@ import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 
 public class DotUtility {
-	
-	public static final int[] COLORS = {Color.RED, Color.BLUE, Color.GREEN, Color.CYAN, Color.MAGENTA, Color.YELLOW, Color.GRAY};
 
-	public static Drawable getDotWithColorIndex(Context context, int colorIndex){
+	public static final String[] HEX_COLORS = { "#ffd800", "#009900", "#004c6d", "#ff0033", "#99cc00", "#ff9900", "#007c6b", "#890E86", "#1170AF", "#893222", "#13cccc", "#cc9900" };
+
+	public static Drawable getDotWithColorIndex(Context context, int colorIndex) {
 		ShapeDrawable dotShape = new ShapeDrawable(new OvalShape());
 
 		int bound = (int) context.getResources().getDimension(R.dimen.dot);
@@ -18,8 +18,8 @@ public class DotUtility {
 		dotShape.setIntrinsicHeight(bound);
 		dotShape.setIntrinsicWidth(bound);
 
-		dotShape.getPaint().setColor(COLORS[colorIndex % COLORS.length]);
-		
+		dotShape.getPaint().setColor(Color.parseColor(HEX_COLORS[colorIndex % HEX_COLORS.length]));
+
 		return dotShape;
 	}
 }
